c6028e620dd6f7e21961c93655b20d259b8f6e42
ApinatF <6162zenon@gmail.com>
2024-01-17 10:14:01 +0700

    Zoom&Rotate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,20 @@ public class CameraController : MonoBehaviour
 {
 
     [SerializeField] private Camera cam;
+    
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+
+    [SerializeField] private float dist;
+    
+    [Header("Rotate")]
+    [SerializeField] private float rotationAmount;
+
+    [SerializeField] private Quaternion newRotation;
 
     [Header("Move")] 
     [SerializeField] private float moveSpeed;
@@ -23,18 +37,27 @@ public class CameraController : MonoBehaviour
     {
         instance = this;
         cam = Camera.main;
+
+        newRotation = transform.rotation;
+        rotationAmount = 1;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 50f;
+
+        zoomSpeed = 25;
+        minZoomDist = 15;
+        maxZoomDist = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveByKB();
+        Zoom();
+        Rotate();
     }
 
     private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
@@ -54,5 +77,45 @@ public class CameraController : MonoBehaviour
 
         transform.position += dir * moveSpeed * Time.deltaTime;
         transform.position = Clamp(corner1.position, corner2.position);
+    }
+    
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+
+        if (Input.GetKey(KeyCode.E))
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * moveSpeed);
+    }
+
+
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+        {
+            zoomModifier = 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            zoomModifier = -0.01f;
+        }
+
+        dist = Vector3.Distance(transform.position, cam.transform.position);
+
+        if (dist < minZoomDist && zoomModifier > 0f)
+        {
+            return;
+        }
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+        {
+            return;
+        }
+
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
+
     }
 }
