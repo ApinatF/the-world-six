b24b6246c44fad28d9be2da7a1b094e9e641b95c
ApinatF <6162zenon@gmail.com>
2024-03-27 13:33:51 +0700

    24.30

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -111,7 +111,7 @@ public class Building : Structure
         if (unitPrefabs[id] == null)
             return;
 
-        GameObject unitObj = Instantiate(unitPrefabs[id], 
+        GameObject unitObj = Instantiate(faction.UnitPrefabs[id], 
             spawnPoint.position, 
             Quaternion.Euler(0f, 180f, 0f),
             faction.UnitsParent);
