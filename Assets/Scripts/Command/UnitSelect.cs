8516f01f8ba3953bd062e95e718c04004035b8df
ApinatF <6162zenon@gmail.com>
2024-04-12 14:37:01 +0700

    25.5

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -26,6 +27,9 @@ public class UnitSelect : MonoBehaviour
     private RectTransform selectionBox;
     private Vector2 oldAnchoredPos;//Box old anchored position
     private Vector2 startPos;//point where mouse is down
+
+    private float timer = 0f;
+    private float timeLimit = 0.5f;
 
     [SerializeField] 
     private Unit curEnemy;
@@ -76,6 +80,14 @@ public class UnitSelect : MonoBehaviour
         {
             ReleaseSelectionBox(Input.mousePosition);
             TrySelect(Input.mousePosition);
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= timeLimit)
+        {
+            timer = 0f;
+            UpdateUI();
         }
 
     }
@@ -217,6 +229,8 @@ public class UnitSelect : MonoBehaviour
         CurUnits.Clear();
 
         curBuilding = null;
+        curResource = null;
+        curEnemy = null;
         
         InfoManager.instance.ClearAllInfo();
         ActionManager.instance.ClearAllInfo();
@@ -268,6 +282,23 @@ public class UnitSelect : MonoBehaviour
     private void ShowEnemyBuilding(Building b)
     {
         InfoManager.instance.ShowEnemyAllInfo(b);
+    }
+    
+    private void UpdateUI()
+    {
+        if (curUnits.Count == 1)
+            ShowUnit(curUnits[0]);
+        else if (curEnemy != null)
+            ShowEnemyUnit(curEnemy);
+        else if (curResource != null)
+            ShowResource();
+        else if (curBuilding != null)
+        {
+            if (GameManager.instance.MyFaction.IsMyBuilding(curBuilding))
+                ShowBuilding(curBuilding);//Show building info
+            else
+                ShowEnemyBuilding(curBuilding);
+        }
     }
     
     
