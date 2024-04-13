b426d97722d3ac68596b0abf82f97d491df764c8
ApinatF <6162zenon@gmail.com>
2024-04-13 14:53:51 +0700

    29.6

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -121,4 +121,9 @@ public class CameraController : MonoBehaviour
         cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
 
     }
+    
+    public void FocusOnPosition(Vector3 pos)
+    {
+        transform.position = pos;
+    }
 }
